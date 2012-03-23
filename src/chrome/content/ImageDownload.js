34a98f28ce8b8a3fcf2b6869295327e94bf2ee11
yetanotherfurry <github-accountkrams@2014.temporarily.de>
2012-03-23 01:20:25 +0100

    Replace variable extensions.faext.download.instantly with extensions.faext.download.autodownload

--- src/chrome/content/ImageDownload.js
+++ src/chrome/content/ImageDownload.js
@@ -117,7 +117,7 @@ com.neocodenetworks.faextender.ImageDownload = {
 		// Handle link onclick event
 		downloadLink.click(com.neocodenetworks.faextender.ImageDownload.DownloadClickEvent);
 		//Auto Download of submissions
-		if (prefs.getBoolPref("extensions.faext.download.instantly")) {
+		if (prefs.getBoolPref("extensions.faext.download.autodownload")) {
 			downloadLink.click();
 		}
 	},
